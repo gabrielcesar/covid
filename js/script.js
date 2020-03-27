997fe6ee5f629afba3886076cf0147e415514d9b
Gabriel Cesar <gabrielcesar2@gmail.com>
2020-03-27 17:40:52 -0300

    Navbar updated.

--- js/script.js
+++ js/script.js
@@ -10,7 +10,22 @@ google.charts.load('current', {
     // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
     'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
 });
-google.charts.setOnLoadCallback(drawRegionsMap);
+
+function drawRegionsMap  () 
+{
+    var data = google.visualization.arrayToDataTable(fetched_data);
+    var options = {
+        colorAxis: {colors: ['#bfe488', 'yellow', '#ff9595']},
+        backgroundColor: 'transparent',
+        fontFamily: 'Roboto Condensed',
+        datalessRegionColor: '#777',
+        defaultColor: '#f5f5f5',
+        tooltip: {isHtml: true}
+    };
+    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+    chart.draw(data, options);
+}
 
 var fetched_data  = [
     ['Country', 'Confirmed', 'Deaths'],
@@ -21,49 +36,30 @@ fetch("https://corona.lmao.ninja/countries")
     .then(response => response.json())
     .then(function(data)
     {
-        for (var country = 0; country < data.length; country ++)
+        console.log(data)
+
+        /*
+        for (let country = 0; country < data.length; country ++)
         {
             if (data[country]['country'] == 'USA')
-            {
                 fetched_data.push([ 'US', data[country]['cases'], data[country]['deaths'] ])
-            }
+            else if (data[country]['country'] == 'UK')
+                fetched_data.push([ 'United Kingdom', data[country]['cases'], data[country]['deaths'] ])
+            else
+                fetched_data.push([ data[country]['country'], data[country]['cases'], data[country]['deaths'] ])
+        }
+        */
+
+        for (let country in data)
+        {
+            if (data[country]['country'] == 'USA')
+                fetched_data.push([ 'US', data[country]['cases'], data[country]['deaths'] ])
             else if (data[country]['country'] == 'UK')
                 fetched_data.push([ 'United Kingdom', data[country]['cases'], data[country]['deaths'] ])
             else
                 fetched_data.push([ data[country]['country'], data[country]['cases'], data[country]['deaths'] ])
         }
 
-        console.log(data)
+        google.charts.setOnLoadCallback(drawRegionsMap);
     })
-
-function drawRegionsMap() 
-{
-    var data = google.visualization.arrayToDataTable(fetched_data);
-    var options = {
-        colorAxis: {colors: ['lightskyblue', 'yellow', '#ff9595']},
-        backgroundColor: 'transparent',
-        datalessRegionColor: '#777',
-        defaultColor: '#f5f5f5',
-        tooltip: {isHtml: true}
-    };
-    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-
-    chart.draw(data, options);
-}
-
-google.charts.setOnLoadCallback(drawRegionsMap);
-
-function open_modal(status)
-{
-    if (status == true)
-    {
-        document.getElementById('arwen_modal_set').style.display = 'block'
-        document.getElementById('arwen_modal').style.display = 'block'
-    }
-    else
-    {
-        document.getElementById('arwen_modal_set').style.display = 'none'
-        document.getElementById('arwen_modal').style.display = 'none'
-    }
-}
 
